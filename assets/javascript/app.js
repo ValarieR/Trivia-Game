c66d43c18cdb4e49c9c96fb1f73fef333acb7598
unknown <Valarie.Regas@gmail.com>
2017-09-05 18:49:39 -0400

    psuedocoded js added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,57 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+//Trivia HW Psuedocode
 
-function buildQuiz(argument) {
-	// body...
-}
+//	At initial load of page
 
-function showResults(argument) {
-	// body...
-}
+$(document).ready(function() {
 
-//display quiz right away
-buildQuiz();
+	//Welcome screen appears
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+	function welcomeImage() {
+    	var img = $("<img>");
+    	img.attr("src", "assets/images/nasty-welcome.png");
+    	$('#image-container').append(img); 
+};
+ welcomeImage();
 
+
+	//Clickable button to begin playing appears
+
+	$("#begin").on("click", function() {
+
+		
+	})
+
+//On click of start
+
+	//Welcome info and button disappear
+
+	//Timer starts, set to 30 secs
+
+	//Question appears
+
+	//4 answers appear
+
+//Player clicks
+
+	//IF player picks correct answer in time, win msg
+
+	//If player picks wrong A, loss msg
+
+	//If time runs out, time out msg
+
+	//Button to continue appears
+
+//Player clicks replays through the loop of possible Q’s
+
+//On last Q
+
+	//Start over button appears
+
+	//Good job msg appears
+
+	//Total count of correct & wrong appear
+
+//Start over cycles back to line 2 of this
+
+
+});
