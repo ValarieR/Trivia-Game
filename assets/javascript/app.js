08eafb22d0fb6ae15ef8122929ca75a6f43540be
unknown <Valarie.Regas@gmail.com>
2017-09-15 17:29:29 -0400

    post-help session

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 
-	$("#questionAnswers").hide();
+    $("#questionAnswers").hide();
 
     var myQuestions = [{
             question: "Who was the first woman to serve on the U.S. Supreme Court?",
@@ -17,7 +17,7 @@ $(document).ready(function() {
         },
         {
             question: "Which civil rights loving woman co-founded a group dedicated to helping homeless young drag queens and trans women of color?",
-            image: "",
+            image: "assets/images/Rivera.png",
             answers: {
                 1: "Mae Jemison",
                 2: "Sylvia Rivera",
@@ -28,7 +28,7 @@ $(document).ready(function() {
         },
         {
             question: "What did Stephanie Kwolek invent in 1965?",
-            image: "",
+            image: "assets/images/Kwolek.jpg",
             answers: {
                 1: "Kevlar",
                 2: "Valium",
@@ -39,7 +39,7 @@ $(document).ready(function() {
         },
         {
             question: "This woman is widely considered to be the first computer programmer, as she created the first algorithm intended to be carried out by a general use computer.",
-            image: "",
+            image: "assets/images/Lovelace.jpg",
             answers: {
                 1: "Mary Somerville",
                 2: "Grace Hopper",
@@ -50,7 +50,7 @@ $(document).ready(function() {
         },
         {
             question: "Constant sex-based discrimination in the work place, and at university, led this woman to dedicate her life to fighting for gender equality. She literally wrote the textbook on gender discrimination, after completing law school while raising a toddler.",
-            image: "",
+            image: "assets/images/Ginsburg.jpg",
             answers: {
                 1: "Sandra Day O'Connor",
                 2: "Ruth Bader Ginsburg",
@@ -61,7 +61,7 @@ $(document).ready(function() {
         },
         {
             question: "This ballerina was not able to tour parts of the American South, for fear of safety due to her race. Despite this sort of limitation, in 1951 she won the Donaldson Award for best dancer on Broadway.",
-            image: "",
+            image: "assets/images/Collins.jpg",
             answers: {
                 1: "Patricia Johnson",
                 2: "Debra Austin",
@@ -72,7 +72,7 @@ $(document).ready(function() {
         },
         {
             question: "This judoka became the first ever American to win Olympic gold; male or female. After winning, she has worked to raise awareness surrounding sexual abuse, and encourages female vicitms to fight back, and thrive.",
-            image: "",
+            image: "assets/images/Harrison.jpg",
             answers: {
                 1: "Kayla Harrison",
                 2: "Sandra Bacher",
@@ -83,7 +83,7 @@ $(document).ready(function() {
         },
         {
             question: "While President of the American Association for the Advancement of Science in 1975 she presided over the passage of a AAAS policy statement deploring discrimination against gay and lesbian scientists. She helped pioneer, through cross-cultural studies, greater understanding for the natural variety of sexual behaviors that occur in human societies.",
-            image: "",
+            image: "assets/images/Mead.jpg",
             answers: {
                 1: "Elizabeth Blackwell",
                 2: "Mary Coble",
@@ -94,7 +94,7 @@ $(document).ready(function() {
         },
         {
             question: "Who was the first Black woman to serve as the US National Security Adviser?. She was also the first Black female to hold the position of provost at Stanford University.",
-            image: "",
+            image: "assets/images/Rice.jpg",
             answers: {
                 1: "Kamala Harris",
                 2: "Condoleezza Rice",
@@ -112,10 +112,11 @@ $(document).ready(function() {
     var wrongCount = 0;
     var unanswered = 0;
 
-    var timeRemaining = 10;
+    var timeRemaining = 15;
     var currentLevel = 0;
 
-console.log(myQuestions[currentLevel].correctAnswer);
+
+    console.log(myQuestions[currentLevel].correctAnswer);
 
 
     //  At initial load of page
@@ -156,9 +157,9 @@ console.log(myQuestions[currentLevel].correctAnswer);
 
 
     function timerReset() {
-        timeRemaining = 10;
+        timeRemaining = 15;
         clearInterval(questionIntervalId);
-        console.log("we runnin the time reset")
+        console.log("we runnin the time reset");
     };
 
     function timerMain() {
@@ -166,7 +167,7 @@ console.log(myQuestions[currentLevel].correctAnswer);
         $("#counter").text("Time Remaining: " + timeRemaining);
         if (timeRemaining < 0) {
             timerReset();
-            //wrongAnswer();
+            wrongAnswer();
         }
     };
 
@@ -179,14 +180,14 @@ console.log(myQuestions[currentLevel].correctAnswer);
     //     createQuestion(currentLevel);
     // }
 
-    function addImage(){
-       //var questionImage = myQuestions[currentLevel].image;
+    function addImage() {
+        //var questionImage = myQuestions[currentLevel].image;
 
-       var newImgDiv = $("<img>");
-       newImgDiv.addClass("show-image img-rounded img-responsive");
-       newImgDiv.attr("src", myQuestions[currentLevel].image);
-       $("#answerImage").append(newImgDiv);
-       //newImgDiv.append(questionImage);
+        var newImgDiv = $("<img>");
+        newImgDiv.addClass("show-image img-rounded img-responsive");
+        newImgDiv.attr("src", myQuestions[currentLevel].image);
+        $("#answerImage").append(newImgDiv);
+        //newImgDiv.append(questionImage);
 
     };
 
@@ -198,9 +199,9 @@ console.log(myQuestions[currentLevel].correctAnswer);
             $(".answers-2").text(myQuestions[x].answers["2"]);
             $(".answers-3").text(myQuestions[x].answers["3"]);
             $(".answers-4").text(myQuestions[x].answers["4"]);
-            $(".timer").text("Time Remaining: " + timeRemaining);
-            
-            $("#answerImage").empty();
+            // $(".timer").text("Time Remaining: " + timeRemaining);
+
+            //$("#answerImage").empty();
 
             questionIntervalId = setInterval(function() {
                 timerMain();
@@ -209,13 +210,13 @@ console.log(myQuestions[currentLevel].correctAnswer);
         } else {
             gameEnd();
         }
-           // console.log(myQuestions[x].answers["1"]);
+        // console.log(myQuestions[x].answers["1"]);
     };
 
 
 
     function correctAnswer() {
-    	var correctAnswer = myQuestions[currentLevel].correctAnswer;
+        var correctAnswer = myQuestions[currentLevel].correctAnswer;
 
         $("#quizQuestions").append("The correct answer was" + myQuestions[currentLevel].correctAnswer);
         answerTimeout = setTimeout(function() {
@@ -224,6 +225,7 @@ console.log(myQuestions[currentLevel].correctAnswer);
         timerReset();
         addImage();
         correctCount++;
+        currentLevel++;
     }
 
     function wrongAnswer() {
@@ -234,6 +236,7 @@ console.log(myQuestions[currentLevel].correctAnswer);
         timerReset();
         addImage();
         wrongCount++;
+        currentLevel++;
     }
 
     function gameEnd() {
@@ -245,27 +248,26 @@ console.log(myQuestions[currentLevel].correctAnswer);
     }
 
     $(".answers").on("click", function() {
-        var yourSelection = $(this).attr("value");
+        var yourSelection = $(this).text();
 
         // console.log('this is my current level ' + myQuestions[currentLevel]);
-        // console.log('this is my correct answer ' + myQuestions[currentLevel].correctAnswer);
-        
+        console.log('this is my correct answer ' + myQuestions[currentLevel].correctAnswer);
+        console.log(yourSelection);
+
         if ((yourSelection === myQuestions[currentLevel].correctAnswer) && (currentLevel < myQuestions.length)) {
 
             correctAnswer();
             timerReset();
             console.log('if statement is true - interating currentLevel')
-            currentLevel++;
+            //currentLevel++;
             createQuestion();
 
-        } 
-
-        else {
+        } else {
 
             wrongAnswer();
-            timerReset();
+            //timerReset();
             console.log('if statement is false - interating currentLevel')
-            currentLevel++;
+            //currentLevel++;
             gameEnd();
         }
     })
